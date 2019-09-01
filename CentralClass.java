8baf3739f03fbf87aad4662b72d99aa942139b3f
socky110 <socky110@gmail.com>
2019-09-01 15:10:20 -0700

    Update CentralClass.java

--- CentralClass.java
+++ CentralClass.java
@@ -26,31 +26,48 @@ public class CentralClass implements KeyListener{
     public void init(){
         
     }
+    public void keyTyped(KeyEvent e){
+        if(e.getKeyCode == KeyEvent.VK_UP){
+            ;
+        }else if(e.getKeyCode == KeyEvent.VK_DOWN){
+            ;
+        }else if(e.getKeyCode == KeyEvent.VK_LEFT){
+            ;
+        }else if(e.getKeyCode == KeyEvent.VK_RIGHT){
+            ;
+        }
+    }
+    public void keyPressed(KeyEvent e){
+        
+    }
+    public void keyReleased(KeyEvent e){
+        
+    }
     static class ImageMake extends Component{
         int x = 0;
         int y = 0;
         
         Image img = null;
         public ImageMake(String src){
-            create(src);
+            make(src);
         }
-        public void create(String src){
+        public void make(String src){
             try{
                 img = ImageIO.read(new File(CentralClass.class.getResource(src).getPath()));
             }catch(IOException e){
                 e.printStackTrace();
             }
         }
-        public int getX(){
+        public int getX2(){
             return x;
         }
-        public int getY(){
+        public int getY2(){
             return y;
         }
-        public void setX(int x1){
-            x = x1;
+        public void setX2(int x2){
+            x = x2;
         }
-        public void setY(int y2){
+        public void setY2(int y2){
             y = y2;
         }
     }
