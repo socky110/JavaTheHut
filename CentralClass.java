8a7756702d39632a02ee3050f04de8e9a49f5af8
socky110 <socky110@gmail.com>
2019-08-31 23:33:35 -0700

    Update CentralClass.java

--- CentralClass.java
+++ CentralClass.java
@@ -1,4 +1,9 @@
-import ;
+import java.awt.Color;
+import java.awt.Graphics; 
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import javax.swing.JFrame;
+
 public class CentralClass{
     public static JFrame jf = new JFrame("Window One");
     
@@ -7,6 +12,7 @@ public class CentralClass{
     }
     public static void main(String[] useless){
         jf.setSize(500, 500);
+        jf.setBackground(Color.ORANGE);
         jf.setVisible(true);
         new CentralClass();
     }
