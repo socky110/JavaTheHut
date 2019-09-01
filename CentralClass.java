37cd61b2260ebf433bd14bb7dc0bf30d4130dd6e
socky110 <socky110@gmail.com>
2019-08-31 23:23:35 -0700

    Update CentralClass.java

--- CentralClass.java
+++ CentralClass.java
@@ -1,11 +1,14 @@
+import ;
 public class CentralClass{
     public static JFrame jf = new JFrame("Window One");
     
     public CentralClass(){
-        
+        this.init();
     }
     public static void main(String[] useless){
         jf.setSize(500, 500);
         jf.setVisible(true);
+        new CentralClass();
     }
+    public
 }
