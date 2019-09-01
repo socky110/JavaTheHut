c44bb781cef9283e16a44b224ed2ad2c3531952e
socky110 <socky110@gmail.com>
2019-09-01 00:02:16 -0700

    Update CentralClass.java

--- CentralClass.java
+++ CentralClass.java
@@ -2,6 +2,9 @@ import java.awt.Color;
 import java.awt.Graphics; 
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.io.File; 
+import java.io.IOException; 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
 public class CentralClass{
@@ -16,5 +19,35 @@ public class CentralClass{
         jf.setVisible(true);
         new CentralClass();
     }
-    public
+    public void init(){
+        
+    }
+    static class ImageMake{
+        int x = 0;
+        int y = 0;
+        
+        Image img = null;
+        public ImageMake(String src){
+            create(src);
+        }
+        public void create(String src){
+            try{
+                img = ImageIO.read(new File(CentralClass.class.getResource(src)));
+            }catch(IOExcepton e){
+                e.printStackTrace();
+            }
+        }
+        public int getX(){
+            return x;
+        }
+        public int getY(){
+            return y;
+        }
+        public void setX(int x1){
+            x = x1;
+        }
+        public void setY(int y2){
+            y = y2;
+        }
+    }
 }
