ffae1bc60d0a08739f3fd9c6e0511c148f782b45
socky110 <socky110@gmail.com>
2019-08-31 23:18:35 -0700

    Create CentralClass.java

---
+++ CentralClass.java
@@ -0,0 +1,11 @@
+public class CentralClass{
+    public static JFrame jf = new JFrame("Window One");
+    
+    public CentralClass(){
+        
+    }
+    public static void main(String[] useless){
+        jf.setSize(500, 500);
+        jf.setVisible(true);
+    }
+}
